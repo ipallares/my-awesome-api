a625e9b3fb280a726f92c696e63761ea2cedf34d
ipallares <ivan@ipallares.com>
2020-12-27 19:17:28 +0100

    add fosrest configuration

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
